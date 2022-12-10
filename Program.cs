b7c847f426e63855e0242930ff3353ab736c0c71
Avaz <qoraqosh1993@mail.ru>
2022-12-11 00:45:00 +0500

    2nd done

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в 
+// натуральную степень B.(Math.Pow использовать нельзя!!!)
+
+
+// void Level(int a, int b)
+// {
+//     int f = 1;
+//     for (int i = 0; i<b; i++)
+//         f = f*a;
+//     Console.WriteLine ($"Level {a} for {b} is {f}");
+// }
+
+// Console.WriteLine("Enter a");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter b");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Level(a, b);
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр 
+// в числе.(Массивы и строки использовать нельзя!!!)
+
+// int Numbers(int a)
+// {
+//     int sum = 0;
+//     int x = 0;
+//     if (a==0)
+//         return 0;
+//     else while (a >= 1 || a <= (-1))
+//     {
+//         x = a%10;
+//         sum = sum +x;
+//         a = a/10;
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine("Enter number ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int result = Numbers(a);
+// Console.WriteLine($"Сумма цифр в числе {a} - {result}");
+
+
+// Напишите программу, которая задаёт массив из произвольного кол-ва 
+// элементов и выводит их на экран.
+
